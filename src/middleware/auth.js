fdc14ec51f29422d107ce9850fc00c482468379a
purchasingpalaquin <purchasingpalaquin@gmail.com>
2026-02-19 12:07:56 +0800

    Add authentication middleware with JWT verification and role-based authorization

---
+++ src/middleware/auth.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const jwt = require('jsonwebtoken');
+
+// Middleware for JWT verification
+const authenticateJWT = (req, res, next) => {
+    const token = req.headers['authorization']?.split(' ')[1];
+    if (!token) {
+        return res.sendStatus(403);
+    }
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+        if (err) {
+            return res.sendStatus(403);
+        }
+        req.user = user;
+        next();
+    });
+};
+
+// Middleware for role-based authorization
+const authorizeRoles = (...roles) => {
+    return (req, res, next) => {
+        if (!req.user || !roles.includes(req.user.role)) {
+            return res.sendStatus(403);
+        }
+        next();
+    };
+};
+
+module.exports = { authenticateJWT, authorizeRoles };
